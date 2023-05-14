22ca302f03ece09929fbc539a8e4075d6fa991a5
mehran feriduni <m3hr4nus@gmail.com>
2023-05-14 08:15:39 +0430

    create documiton

--- app.js
+++ app.js
@@ -4,14 +4,30 @@ const flash = require('connect-flash');
 
 const connect = require('./Data/connect');
 
-require('dotenv').config({path:'./config/config.env'});
+require('dotenv').config({ path: './config/config.env' });
 
-const app=express();
+const app = express();
 
 
 //!database connct
 connect();
 
+/**
+ * 
+ * @param {String} name the username person
+ * @param {tel} tell the teel number person 
+ * @param {Number} code the code secsess
+ * @returns {object} return object
+ */
+function creatperson(name, tell, code) {
+
+  return {
+    name,
+    tell,
+    code,
+  }
+
+}
 
 //* BodyPaser
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +38,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(session({
-  secret:'geeksforgeeks',
+  secret: process.env.SESSIONCODE,
   saveUninitialized: true,
   resave: true
 }));
@@ -32,9 +48,9 @@ app.use(flash())
 app.use(express.static('public'))
 
 //!router
-app.use("/",require('./routers/user'))
+app.use("/", require('./routers/user'))
 app.use(require("./controllers/errorc").get404);
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
   console.log("start");
 })
